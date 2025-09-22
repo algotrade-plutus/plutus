65496acd35967083899118884ab89d3737d1e505
Dân <andan@algotrade.vn>
2025-09-22 10:00:28 +0700

    Added Historical Performance test cases

--- src/plutus/evaluation/performance.py
+++ src/plutus/evaluation/performance.py
@@ -7,121 +7,112 @@ from decimal import Decimal
 from typing import List, Tuple
 
 
-class Performance:
-    """Performance in a period of an investment
-    Args:
-        absolute_return (Decimal): return of this investment in the period
-    """
-    absolute_return: Decimal
-
-
-@dataclass
-class IndexPerformance(Performance):
-    """Performance of an index"""
-    index: str
-    period_return: Decimal
-    absolute_return: Decimal = field(init=False)
-
-    def __post_init__(self):
-        self.absolute_return = self.period_return
-
-
-class PerformanceHistory:
-    """History of an investment"""
+class HistoricalPerformance:
+    """Historical Performance calculator"""
     def __init__(
-        self, dates: List[Tuple[datetime.date, ...]],
-        performances: List[Performance],
-        annualized_factor: Decimal
+        self,
+        returns: List[Decimal],
+        annualized_factor: Decimal,
+        risk_free_return: Decimal,
+        minimal_acceptable_return: Decimal
     ):
         """
         Init a performance history of an investment with a list of performance of consecutive periods
 
         Args:
-            dates: a list of dates that define the consecutive periods of an investment, two consecutive dates in the
-                   list are in turn the start and end dates of a period
-            performances: list of performances that correspond to periods defined by argument dates
-            annualized_factor: the number of periods in a year
+            returns (List of Decimal): List of returns
+            annualized_factor (Decimal): The number of periods in a year
+            risk_free_return (Decimal): The risk-free return
+            minimal_acceptable_return (Decimal): The minimal acceptable return
         """
-        self.dates = dates
-        self.performances = performances
-        self.n_periods = len(performances)
+        # self.dates = dates
+        self.returns = returns
+        self.num_return = len(self.returns)
         self.annualized_factor = annualized_factor
-        self.sharpe_ratio = self._get_sharpe_ratio()
-        self.sortino_ratio = self._get_sortino_ratio(mar=Decimal('0.07'))
-        self.cumulative_performance = self._get_cumulative_performance()
+        self.risk_free_return = risk_free_return
+        self.minimal_acceptable_return = minimal_acceptable_return
+        self.return_mean = statistics.mean(self.returns)
+        self.return_std = statistics.stdev(self.returns)
+
+        self.sharpe_ratio = self._get_sharpe_ratio(risk_free_return=self.risk_free_return)
+        self.sortino_ratio = self._get_sortino_ratio(minimal_acceptable_return=Decimal('0.07'))
+        self.cumulative_performances = self._get_cumulative_performances()
         self.maximum_drawdown = self._get_maximum_drawdown()
         self.annual_return = self._get_annual_return()
-        self.longest_drawdown = self._get_longest_drawdown()
+        self.longest_drawdown_period = self._get_longest_drawdown_period()
 
-    def _get_sharpe_ratio(self) -> Decimal:
-        """Compute sharpe ratio of this history of investment"""
-        returns = [performance.absolute_return for performance in self.performances]
-        if len(returns) <= 1 or all(r == Decimal('0') for r in returns):
+    def _get_sharpe_ratio(self, risk_free_return: Decimal) -> Decimal:
+        """Computes sharpe ratio of this history of return"""
+        if len(self.returns) <= 1 or all(r == Decimal('0') for r in self.returns):
             return Decimal('0')
-        risk_free_return = Decimal('0.03')
-        return Decimal(self.annualized_factor**Decimal(1/2))*(
-                statistics.mean(returns)-risk_free_return/self.annualized_factor
-        )/statistics.stdev(returns)
 
-    def _get_sortino_ratio(self, mar: Decimal) -> Decimal:
-        """Compute sortino ratio of this history of investment"""
-        returns = [performance.absolute_return for performance in self.performances]
-        if len(returns) <= 1 or all(r == Decimal('0') for r in returns):
-            return Decimal('0')
-        target_semi_deviation = Decimal(math.pow(
-            sum(min(Decimal(0), r - mar/self.annualized_factor)**2 for r in returns)/len(returns), Decimal('0.5')
-        ))
         return (
-            Decimal(self.annualized_factor**Decimal(1/2))*(
-                    statistics.mean(returns)-mar/self.annualized_factor
-            )/target_semi_deviation
-            if target_semi_deviation > 0 else Decimal('Inf')
+            self.annualized_factor**Decimal('0.5') *
+            (self.return_mean - risk_free_return/self.annualized_factor) / self.return_std
         )
 
-    def _get_cumulative_performance(self) -> List[Decimal]:
-        """Compute cumulative performance of this history of investment"""
-        cumulative_performance = [Decimal('1')]
-        for performance in self.performances:
-            cumulative_performance.append(
-                cumulative_performance[-1]*(1 + performance.absolute_return)
+    def _get_sortino_ratio(self, minimal_acceptable_return: Decimal) -> Decimal:
+        """Computes sortino ratio of this history of return"""
+        if len(self.returns) <= 1 or all(r == Decimal('0') for r in self.returns):
+            return Decimal('0')
+
+        downside_deviation = (
+            statistics.mean(
+                [min(Decimal(0), r - minimal_acceptable_return / self.annualized_factor)**2 for r in self.returns]
+            ) ** Decimal('0.5')
+        )
+        return (
+            self.annualized_factor**Decimal('0.5') *
+            (self.return_mean - minimal_acceptable_return/self.annualized_factor) /
+            downside_deviation
+            if downside_deviation > 0 else Decimal('Inf')
+        )
+
+    def _get_cumulative_performances(self) -> List[Decimal]:
+        """Computes cumulative performances of this history of return"""
+        cumulative_performances = [Decimal('1')]
+        for r in self.returns:
+            cumulative_performances.append(
+                cumulative_performances[-1] * (1 + r)
             )
-        return cumulative_performance
+        return cumulative_performances
 
     def _get_maximum_drawdown(self) -> Decimal:
-        """Compute maximum drawdown of this history of investment"""
+        """Computes maximum drawdown of this history of return"""
         max_performance = [
-            max(self.cumulative_performance[:i + 1])
-            for i in range(len(self.cumulative_performance))
+            max(self.cumulative_performances[:i + 1])
+            for i in range(len(self.cumulative_performances))
         ]
 
-        dd = [
-            (self.cumulative_performance[i] - max_performance[i])/max_performance[i]
+        drawdowns = [
+            (self.cumulative_performances[i] - max_performance[i])/max_performance[i]
             for i in range(len(max_performance))
         ]
 
-        return min(dd)
+        # min since the drawdowns are negative. The lowest drawdown == maximum drawdown.
+        return min(drawdowns)
 
     def _get_annual_return(self) -> Decimal:
-        """Compute annual return of this history of investment"""
-        return Decimal(
-            math.pow(self.cumulative_performance[-1], self.annualized_factor/len(
-                self.performances
-            ))
-        ) - 1
+        """Computes annual return of this history of return"""
+        return (
+            self.cumulative_performances[-1] ** (self.annualized_factor/self.num_return) - 1
+        )
 
-    def _get_longest_drawdown(self) -> int:
-        """Compute the longest drawdown of this history of investment"""
-        longest_drawdown = 0
+    def _get_longest_drawdown_period(self) -> int:
+        """Computes the longest drawdown of this history of return"""
+        longest_drawdown_period = 0
         max_performance = 1
         max_performance_index = 0
         min_performance = 1
-        for i in range(1, len(self.cumulative_performance)):
-            if self.cumulative_performance[i] > max_performance:
-                max_performance = self.cumulative_performance[i]
+
+        for i in range(1, len(self.cumulative_performances)):
+            if self.cumulative_performances[i] > max_performance:
+                max_performance = self.cumulative_performances[i]
                 min_performance = max_performance
                 max_performance_index = i
             else:
-                if self.cumulative_performance[i] < min_performance:
-                    min_performance = self.cumulative_performance[i]
-                    longest_drawdown = max(longest_drawdown, i - max_performance_index)
-        return longest_drawdown
+                if self.cumulative_performances[i] < min_performance:
+                    min_performance = self.cumulative_performances[i]
+                    longest_drawdown_period = max(longest_drawdown_period, i - max_performance_index)
+
+        return longest_drawdown_period
